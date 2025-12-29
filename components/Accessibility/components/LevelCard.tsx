9b151c6c5861c3354725c9e3b3684d9f14186fd1
elvinmurshudlu-mina <elvin.murshudlu@mina.az>
2025-12-29 10:30:58 +0400

    Accesbility

--- components/Accessibility/components/LevelCard.tsx
+++ components/Accessibility/components/LevelCard.tsx
@@ -39,14 +39,14 @@ function LevelCard({
             className={clsx(
                 "flex select-none flex-col items-center transition-all duration-150 gap-2 cursor-pointer border-2  p-4 rounded-lg hover:border-blue-500",
                 {
-                    'border-blue-500': currentLevel !== -1,
-                    'border-gray-300':currentLevel === -1,
-                    'text-blue-500': currentLevel !== -1,
-                }
+                    "border-blue-500": currentLevel !== -1,
+                    "border-gray-300": currentLevel === -1,
+                    "text-blue-500": currentLevel !== -1,
+                },
             )}
         >
-            <div className={'text-2xl'}>{current.icon}</div>
-            <h5 className={'text-base'}>{current.title}</h5>
+            <div className={"text-2xl"}>{current.icon}</div>
+            <h5 className={"text-base"}>{current.title}</h5>
             <LevelBadge
                 levels={levels}
                 current={currentLevel}
@@ -69,10 +69,15 @@ function LevelBadge({
     }, [current, levels])
 
     return (
-        <div className={clsx("w-full flex gap-1 transition ",{
-            'opacity-0': current === -1,
-            'opacity-100': current !== -1
-        })}>
+        <div
+            className={clsx(
+                "w-full flex gap-1 transition ",
+                {
+                    "opacity-0": current === -1,
+                    "opacity-100": current !== -1,
+                },
+            )}
+        >
             {levels.map((_, i) => (
                 <span
                     key={i}
