ff161ac97d302abb03fea43cdef944cb8b8521e9
elvinmurshudlu-mina <elvin.murshudlu@mina.az>
2025-12-30 10:51:43 +0400

    Redirect locale

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,10 +2,15 @@ import { Link } from "@/i18n/navigation"
 
 function Header() {
     return (
-        <div className={"w-full flex gap-4"}>
-            <Link href={"/"}>Ana səhifə</Link>
-            <Link href={"/about"}>Haqqinda</Link>
-        </div>
+        <nav className={"bg-primary-color"}>
+            <div className={"section"}>nav logo</div>
+            <div className={"section"}>
+                <div className={"w-full flex gap-4"}>
+                    <Link href={"/"}>Ana səhifə</Link>
+                    <Link href={"/about"}>Haqqinda</Link>
+                </div>
+            </div>
+        </nav>
     )
 }
 
